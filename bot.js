b9e740d82027c9bc81abf06c862ee8954cb0e66a
Sellmox5 <42848880+SPLITLPTEOEORK2003@users.noreply.github.com>
2018-12-13 11:51:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -210,7 +210,7 @@ function play(guild, song) {
 }//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
 
 const adminprefix = ".";//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
-const devs = ['521001769082552335'];//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+const devs = ['353708472690540564'];//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
 client.on('message', message => {//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
   var argresult = message.content.split(` `).slice(1).join(' ');//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
     if (!devs.includes(message.author.id)) return;//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
